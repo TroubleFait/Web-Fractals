a223c7b62bab95a1363df3c9e296c927ad20bf2d
troublefait <131655771+TroubleFait@users.noreply.github.com>
2026-01-09 23:55:25 +0000

    Shaders

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,40 @@
 // fragment shader
 precision mediump float;
-varying vec2 v_uv;
+// varying vec2 v_uv;
+varying vec2    v_c; // the actual complex coordinate
+const int       MAX_ITER = 100;
+
+vec2 cMul(vec2 a, vec2 b) {
+    // return mat2(a, -a.y, a.x) * b;
+    return vec2(
+        a.x * b.x - a.y * b.y,
+        a.x * b.y + a.y * b.x
+    );
+}
+
+float cSquaredAbs(vec2 z) {
+    // return Math.sqrt(z.re * z.re + z.im * z.im);
+    return dot(z, z);
+}
+
+vec2 f(vec2 z, vec2 c) {
+    return cMul(z, z) + c;
+}
+
+int iterations(vec2 c, int max) {
+    vec2 z = vec2(0.0);
+    int n;
+    for (n = 0; n < max; ++n) {
+        if (cSquaredAbs(z) > 4.0) return n;
+        z = f(z, c);
+    }
+    return max;
+}
 
 void main() {
-    // Red gradient from left to right, green from top to bottom
-    gl_FragColor = vec4(v_uv.x, v_uv.y, 0.0, 1.0);
+    // // Red gradient from left to right, green from top to bottom
+    // gl_FragColor = vec4(v_uv.x, v_uv.y, 0.0, 1.0);
+
+    float   brightness = float(iterations(v_c, MAX_ITER)) / MAX_ITER;
+    gl_FragColor = vec4(brightness, brightness, brightness, 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,11 +1,21 @@
 // Vertex shader
-attribute vec2 a_position;
-varying vec2 v_uv;
+// attribute vec2 a_position;
+// varying vec2 v_uv;
+varying vec2 v_c; // the actual complex coordinate
+uniform float xmin, xmax, ymin, ymax; // the viewport in the complex plane
 
 void main() {
-    // Pass normalized coordinates to fragment shader
-    v_uv = a_position * 0.5 + 0.5; // map from [-1,1] to [0,1]
+    // // Pass normalized coordinates to fragment shader
+    // v_uv = a_position * 0.5 + 0.5; // map from [-1,1] to [0,1]
 
-    // gl_Position must be vec4
+    // // gl_Position must be vec4
+    // gl_Position = vec4(a_position, 0.0, 1.0);
+
+    vec2    uv = a_position * 0.5 + 0.5; // map from [-1,1] to [0,1]
+    v_c = vec2(
+        mix(xmin, xmax, uv.x),
+        mix(ymin, ymax, uv.y)
+    );
+
     gl_Position = vec4(a_position, 0.0, 1.0);
 }
