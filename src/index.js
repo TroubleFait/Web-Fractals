4c031c080f4e9d31fc92fe9dbf5532c5cbc8a4a1
troublefait <131655771+TroubleFait@users.noreply.github.com>
2026-01-09 18:41:25 +0000

    Fixed canvas size

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ main();
 
 function main() {
   const canvas = document.querySelector("#glCanvas");
+  canvas.width = window.innerWidth * window.devicePixelRatio;
+  canvas.height = window.innerHeight * window.devicePixelRatio;
 
   const gl = canvas.getContext("webgl");
   if (!gl) {
@@ -11,6 +13,8 @@ function main() {
     return;
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 0.0, 0.0, 1.0); // DEBUG: red
+  // gl.clearColor(0.0, 0.0, 0.0, 1.0); // PRODUCTION: black
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
