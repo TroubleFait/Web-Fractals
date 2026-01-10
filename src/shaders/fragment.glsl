f3bbcd1555d889fb5b1246d2a734624477393f79
troublefait <131655771+TroubleFait@users.noreply.github.com>
2026-01-10 17:36:15 +0000

    constant loop limit

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -21,11 +21,11 @@ vec2 f(vec2 z, vec2 c) {
     return cMul(z, z) + c;
 }
 
-int iterations(vec2 c, int max) {
+int iterations(vec2 c) {
     vec2    z = vec2(0.0);
     int     n = 0;
 
-    for (int i = 0; i < max; ++i) {
+    for (int i = 0; i < MAX_ITER; ++i) {
         if (cSquaredAbs(z) > 4.0) break;
         z = f(z, c);
         ++n;
@@ -38,7 +38,7 @@ void main() {
     // // Red gradient from left to right, green from top to bottom
     // gl_FragColor = vec4(v_uv.x, v_uv.y, 0.0, 1.0);
 
-    // float   brightness = float(iterations(v_c, MAX_ITER)) / MAX_ITER;
+    // float   brightness = float(iterations(v_c)) / MAX_ITER;
     // gl_FragColor = vec4(brightness, brightness, brightness, 1.0);
 
     gl_FragColor = vec4(
